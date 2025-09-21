221e10c782316b3f573f3ea609e1004713f95b0d
Petr Havlát <petr.havlat@outlook.com>
2025-09-21 20:18:06 +0200

    Create PersonViewModel.cs

---
+++ samples/ToneGenerator/MvvmTest/PersonViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Havlat.CsharpSpellbook.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToneGenerator.MvvmTest
+{
+	public class PersonViewModel : ObservableObject
+	{
+		public string FirstName { get => GetValue<string>(); set => SetValue(value, new string[] { "FirstName", "FullName" }); } 
+		public string FamilyName { get => GetValue<string>(); set => SetValue(value, new string[] { "FamilyName", "FullName" }); } 
+		public string FullName { get => $"{FirstName} {FamilyName}"; }
+
+		public PersonViewModel() =>	InitializePropertiesUsingReflection();
+
+		public PersonViewModel(string firstName, string lastName)
+		{
+			InitializePropertiesUsingReflection();
+			
+			FirstName = firstName;
+			FamilyName = lastName;
+		}		
+	}
+}
