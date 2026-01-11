7d86d5fefd74bebe15cacea1275334ce9095ccd5
Petr Havlát <petr.havlat@outlook.com>
2026-01-11 09:39:28 +0100

    Create ByteOrderSwapper.cs

---
+++ CSharpSpellbook.Numbers/ByteOrderSwapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpSpellbook.Numbers
+{
+	/*
+     
+    Endianita (pořadí bajtů, anglicky byte order) je v informatice způsob uložení čísel v operační paměti počítače, 
+    který definuje, v jakém pořadí se uloží jednotlivé bajty číselného datového typu. Jde tedy o to, 
+    v jakém pořadí jsou v operační paměti uloženy jednotlivé řády čísel, které zabírají více než jeden bajt.
+    
+	*/
+	public static class ByteOrderSwapper
+    {
+
+    }
+}
