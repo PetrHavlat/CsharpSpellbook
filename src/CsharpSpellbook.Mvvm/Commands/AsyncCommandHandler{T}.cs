7c89084da74676a672e2803ea7a9e6cd96000912
Petr Havlát <petr.havlat@outlook.com>
2026-01-11 09:39:42 +0100

    Update AsyncCommandHandler{T}.cs

--- src/CsharpSpellbook.Mvvm/Commands/AsyncCommandHandler{T}.cs
+++ src/CsharpSpellbook.Mvvm/Commands/AsyncCommandHandler{T}.cs
@@ -11,112 +11,64 @@ namespace Havlat.CsharpSpellbook.Mvvm.Commands
 	/// Provides a generic asynchronous command functionality.
 	/// </summary>
 	/// <typeparam name="T">The type of the command parameter.</typeparam>
-	public class AsyncCommandHandler<T> : ICommand
+	public class AsyncRelayCommand<T> : ICommand
 	{
-		#region Fields
-
-		private readonly Func<bool> _canExecute;
 		private readonly Func<T, Task> _execute;
-		private bool _isExecuting;
+		private readonly Func<T, bool>? _canExecute;
+		private readonly SemaphoreSlim _semaphore = new(1, 1);
+		private CancellationTokenSource? _cts;
 
-		#endregion
-
-		#region Constructors
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="CommandHandler{T}"/> class.
-		/// </summary>
-		/// <param name="execute">The asynchronous function to execute when the command is invoked.</param>
-		/// <param name="canExecute">The function that determines whether the command can execute.</param>
-		/// <exception cref="ArgumentNullException">Thrown when execute is null.</exception>
-		public AsyncCommandHandler(Func<T, Task> execute, Func<bool> canExecute)
+		public AsyncRelayCommand(Func<T, Task> execute, Func<T, bool>? canExecute = null)
 		{
 			_execute = execute ?? throw new ArgumentNullException(nameof(execute));
 			_canExecute = canExecute;
 		}
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="CommandHandler{T}"/> class.
-		/// </summary>
-		/// <param name="execute">The asynchronous function to execute when the command is invoked.</param>
-		/// <exception cref="ArgumentNullException">Thrown when execute is null.</exception>
-		public AsyncCommandHandler(Func<T, Task> execute)
-			: this(execute, null)
+		public event EventHandler? CanExecuteChanged;
+
+		public bool CanExecute(object? parameter)
 		{
-		}
-
-		#endregion
-
-		#region ICommand Implementation
-
-		/// <summary>
-		/// Occurs when changes occur that affect whether or not the command should execute.
-		/// </summary>
-		public event EventHandler CanExecuteChanged;
-
-		/// <summary>
-		/// Determines whether the command can execute in its current state.
-		/// </summary>
-		/// <param name="parameter">Data used by the command.</param>
-		/// <returns>True if this command can be executed; otherwise, false.</returns>
-		public bool CanExecute(object parameter)
-		{
-			if (!_isExecuting && (_canExecute == null || _canExecute()))
-				return true;
-
-			if (parameter == null && typeof(T).IsValueType)
+			if (parameter is not T typedParam)
 				return false;
 
-			return _canExecute(parameter);
+			return !_semaphore.CurrentCount.Equals(0) &&
+				   (_canExecute?.Invoke(typedParam) ?? true);
 		}
 
-		/// <summary>
-		/// Executes the command.
-		/// </summary>
-		/// <param name="parameter">Data used by the command.</param>
-		public async void Execute(object parameter)
+		public async void Execute(object? parameter)
 		{
-			var exec = _isExecuting ? 1 : 0;
-
-			if (!CanExecute(parameter)) return;
-
-			// Atomic check and set
-			if (Interlocked.CompareExchange(ref exec, 1, 0) != 0)
+			if (!CanExecute(parameter) || parameter is not T typedParam)
 				return;
+
+			_cts?.Cancel();
+			_cts = new CancellationTokenSource();
 
 			try
 			{
+				await _semaphore.WaitAsync(_cts.Token);
 				RaiseCanExecuteChanged();
-				await _execute((T)parameter);
+				await _execute(typedParam);
 			}
+			catch (OperationCanceledException) { }
 			finally
 			{
-				Interlocked.Exchange(ref exec, 0);
+				_semaphore.Release();
 				RaiseCanExecuteChanged();
+				_cts = null;
 			}
 		}
-
-		#endregion
-
-		#region Methods
 
 		/// <summary>
 		/// Raises the <see cref="CanExecuteChanged"/> event.
 		/// </summary>
-		public void RaiseCanExecuteChanged()
-		{
-			OnCanExecuteChanged(EventArgs.Empty);
-		}
+		public void RaiseCanExecuteChanged() =>
+			CanExecuteChanged?.Invoke(this, EventArgs.Empty);
 
-		/// <summary>
-		/// Raises the <see cref="CanExecuteChanged"/> event with the provided arguments.
-		/// </summary>
-		/// <param name="e">Arguments of the event being raised.</param>
-		protected virtual void OnCanExecuteChanged(EventArgs e)
+		public void Dispose()
 		{
-			CanExecuteChanged?.Invoke(this, e);
+			_cts?.Cancel();
+			_semaphore.Dispose();
 		}
-
-		#endregion
 	}
+
 }
